787703caf5c976b673075d2332a460afb1d02df7
C-NASIR <cnasir22.0@gmail.com>
2023-02-21 19:03:11 -0600

    created a temp user for testing posts

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 import express from "express";
 import sequelize from "./util/database.js";
 import { connectModels } from "./util/associations.js";
+import User from "./models/user.js";
+
 const app = express();
 
 // body parser
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.disable("x-powered-by"); // do not identify Express
+
+app.use((req, res, next) => {
+  //temp needed for posts
+  User.findByPk(1)
+    .then((user) => {
+      req.body.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+      next();
+    });
+});
 
 app.use((req, res) => {
   res.render("home");
